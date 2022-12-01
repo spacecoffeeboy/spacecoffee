3249888d76083d8d22e8ccb98b125756433a1419
spacecoffeeboy <117617635+spacecoffeeboy@users.noreply.github.com>
2022-12-01 13:47:18 +0900

    Add files via upload

---
+++ bottom.php
@@ -0,0 +1,4 @@
+    <footer>
+            HRDKOREA Copyright &copy; 2018 All rights reserved. Human Resources Development Service of Korea
+    </footer>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<html>
-    <div align="center">
-        <br><br>
-        <h2>학생 성적처리</h2>
-        
-        <a href="list.php">목록보기</a> <br>
-        <a href="form.php">저장보기</a> <br>
-
-    </div>
-</html>
+<?include("top.php");?>
+        <section>
+        <br>
+        <div align="center">
+        <h2>고등학교 성적 조회 프로그램</h2>
+        </div>
+        <div>
+            고등학교 성적을 조회하는 프로그램을 작성한다. <br><br>
+            &emsp;1. 학생성적정보 테이블을 생성한다.<br><br>
+            &emsp;2. 학생성적정보에 샘플데이터를 SQL문을 사용하여 데이터를 추가생성한다.<br><br>
+            &emsp;3. 주어진 화면 디자인을 이용하여 화면을 디자인한다.<br><br>
+            &emsp;4. 업무요건에 따라 프로그램을 작성한다.<br><br>
+        </div>
+        <br>    
+        </section>
+    </body>
+<?include("bottom.php");?>
